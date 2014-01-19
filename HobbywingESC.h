dcb9188c1d9e9f3b9f649674fde62d9924457b88
christianrahn <github@christianrahn.us>
2014-01-19 08:22:44 -0500

    change extensions to appropriate arduino library

---
+++ HobbywingESC.h
@@ -0,0 +1,11 @@
+/*
+  HobbywingESC.h - Library for manipulating a HobbywingESC without the typical RC control
+  Created by Christian Rahn, Jan 19, 2014.
+  Open release to the public - enjoy!
+  
+*/
+
+#ifndef HobbywingESC.h
+#define HobbywingESC.h
+
+#include "Arduino.h"
